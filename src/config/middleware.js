6b7b921b99a13f70666502079539799a844c9f1e
coolify-host <coolify-host@local>
2026-05-18 02:21:40 +0000

    fix: always enable resource middleware so /static/ serves files in production

--- src/config/middleware.js
+++ src/config/middleware.js
@@ -11,7 +11,7 @@ module.exports = [
   },
   {
     handle: 'resource',
-    enable: isDev,
+    enable: true,  // ops: always serve /static/ (saw211 跑 NODE_ENV=dev,我们 prod 也必须开)
     options: {
       root: path.join(think.ROOT_PATH, 'www'),
       publicPath: /^\/(static|favicon\.ico)/
